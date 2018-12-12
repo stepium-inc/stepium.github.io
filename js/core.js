79eea62279dbe0be501fa7c137ddc2e1c01c48ce
yurii <yuriy.levytskyy@gmail.com>
2018-12-12 17:10:13 +0200

    add files

---
+++ js/core.js
@@ -0,0 +1,21 @@
+
+function Stepium() {
+	var that = this;
+
+	this.init = function () {
+		this.initModules();
+
+	},
+	this.initModules = function () {
+
+
+	}
+};
+
+$( document ).ready(function() {
+	var main = new Stepium();
+	main.init();
+});
+
+
+
