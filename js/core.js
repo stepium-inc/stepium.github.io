4d05118f71a22ae8193e5bb866b779186956e60c
yurii <yuriy.levytskyy@gmail.com>
2018-12-17 15:04:38 +0200

    develop register

--- js/core.js
+++ js/core.js
@@ -8,9 +8,61 @@ function Stepium() {
 	},
 	this.initModules = function () {
 
+        $(".send-form-data").on('click',function(){
+            var order = {};
+            var a = $(this);
+
+            a.closest('form').find('.name').removeClass('error');
+            a.closest('form').find('.email').removeClass('error');
+
+            order.username = a.closest('form').find('.name').val();
+            order.email = a.closest('form').find('.email').val();
+            order.phone = a.closest('form').find('.phone').val();
+
+            var error0 = false;
+
+            if (order.username=='') {
+                a.closest('form').find('.name').addClass('error');
+                error0 = true;
+            }
+
+            var r = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            if (!r.test(order.email)) {
+                a.closest('form').find('.email').addClass('error');
+                error0 = true;
+            }
+
+            if(!error0){
+            	var body = {
+                    username : order.username,
+                    email : order.email,
+                    phone : order.phone
+				};
+                $.ajax({
+                    type: 'POST',
+                    url: 'http://api.dev.kylbaba.com/api/v2/users/create/user/landing/',
+                    data: body,
+					success: function(result){
+						alert('Регистрация прошла успешно! На указанный вами e-mail '+ order.email +' было отправлено письмо с подтверждением регистрации!');
+						a.closest('form').find('.name').val('');
+						a.closest('form').find('.email').val('');
+						a.closest('form').find('.phone').val('');
+                    },
+                    error: function (error) {
+						if(error['responseJSON']['code'] == 105){
+                            alert('Ошибка! Такой email уже существует');
+						}else{
+                            alert('Ошибка! ' + error['responseJSON']['message']);
+						}
+                    },
+                    dataType: 'json'
+                });
+            }
+            return false;
+        });
 
 	}
-};
+}
 
 $( document ).ready(function() {
 	var main = new Stepium();
